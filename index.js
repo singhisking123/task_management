e2f2f43cae7ce346fdc107c792f2aa5e7cc5a37e
singhisking123 <singhiskingdeepankar@gmail.com>
2022-05-23 22:54:33 +0530

    Here's my first front end code

--- index.js
+++ index.js
@@ -16,51 +16,10 @@ const addNewCard = () => {
   console.log(taskData);
 
   // generate the HTML code
-  const newTask = `<div id=${taskData.id} class="col-md-6 col-lg-4 my-4">
-  <div class="card">
-    <div class="card-header gap-2 d-flex justify-content-end">
-      <button class="btn btn-outline-info" type="submit">
-        <i class="fa-solid fa-pencil"></i>
-        <!--upar wla Code is for the icon(pencil)-->
-      </button>
-      <button class="btn btn-outline-danger" type="submit">
-        <i class="fa-solid fa-trash"></i>
-        <!--upar wla Code is for the icon(delete)-->
-      </button>
-    </div>
-    <img
-      src=${taskData.image}
-      class="card-img-top"
-      alt="TaskImage"
-    />
-    <div class="card-body" style="background-color: rgb(184, 236, 184)">
-      <h5
-        class="task_card_title"
-        style="background-color: rgb(79, 237, 68)"
-      >
-        ${taskData.title}
-      </h5>
-      <p
-        class="description trim-3-lines text-muted"
-        style="background-color: rgb(235, 117, 117)"
-      >
-        ${taskData.description}
-      </p>
-    </div>
-    <div class="tags text-white" style="background-color: red">
-      <h4>
-        <span class="badge bg-primary">${taskData.type}</span>
-      </h4>
-    </div>
-
-    <div class="card-footer text-muted">
-      <a href="#" class="btn btn-outline-primary">Open Task</a>
-    </div>
-  </div>
-</div>`;
+  const newTask = generateHTML(taskData);
 
   // inject in the DOM
-  taskContainer.insertAdjacentHTML("beforeend", newTask);
+  insertToDOM(newTask);
 
   // clear the FORM
   document.getElementById("taskTitle").value = "";
@@ -85,49 +44,58 @@ const loadExistingCards = () => {
   // Now traverse the array using map AND FOREACH bcoze it can change the data
   globalTaskData.map((task) => {
     // generate HTML code for those data
-    const newCard = `<div id=${task.id} class="col-md-6 col-lg-4 my-4">
-    <div class="card">
-      <div class="card-header gap-2 d-flex justify-content-end">
-        <button class="btn btn-outline-info" type="submit">
-          <i class="fa-solid fa-pencil"></i>
-          <!--upar wla Code is for the icon(pencil)-->
-        </button>
-        <button class="btn btn-outline-danger" type="submit">
-          <i class="fa-solid fa-trash"></i>
-          <!--upar wla Code is for the icon(delete)-->
-        </button>
-      </div>
-      <img
-        src=${task.image}
-        class="card-img-top"
-        alt="TaskImage"
-      />
-      <div class="card-body" style="background-color: rgb(184, 236, 184)">
-        <h5
-          class="task_card_title"
-          style="background-color: rgb(79, 237, 68)"
-        >
-          ${task.title}
-        </h5>
-        <p
-          class="description trim-3-lines text-muted"
-          style="background-color: rgb(235, 117, 117)"
-        >
-          ${task.description}
-        </p>
-      </div>
-      <div class="tags text-white" style="background-color: red">
-        <h4>
-          <span class="badge bg-primary">${task.type}</span>
-        </h4>
-      </div>
-  
-      <div class="card-footer text-muted">
-        <a href="#" class="btn btn-outline-primary">Open Task</a>
-      </div>
-    </div>
-  </div>`;
+    const newCard = generateHTML(task);
+
     // inject to the DOM
-    taskContainer.insertAdjacentHTML("beforeend", newCard);
+    insertToDOM(newCard);
   });
+};
+
+const generateHTML = (
+  task
+) => `<div id=${task.id} class="col-md-6 col-lg-4 my-4">
+  <div class="card">
+    <div class="card-header gap-2 d-flex justify-content-end">
+      <button class="btn btn-outline-info" type="submit">
+        <i class="fa-solid fa-pencil"></i>
+        <!--upar wla Code is for the icon(pencil)-->
+      </button>
+      <button class="btn btn-outline-danger" type="submit">
+        <i class="fa-solid fa-trash"></i>
+        <!--upar wla Code is for the icon(delete)-->
+      </button>
+    </div>
+    <img
+      src=${task.image}
+      class="card-img-top"
+      alt="TaskImage"
+    />
+    <div class="card-body" style="background-color: rgb(184, 236, 184)">
+      <h5
+        class="task_card_title"
+        style="background-color: rgb(79, 237, 68)"
+      >
+        ${task.title}
+      </h5>
+      <p
+        class="description trim-3-lines text-muted"
+        style="background-color: rgb(235, 117, 117)"
+      >
+        ${task.description}
+      </p>
+    </div>
+    <div class="tags text-white" style="background-color: red">
+      <h4>
+        <span class="badge bg-primary">${task.type}</span>
+      </h4>
+    </div>
+
+    <div class="card-footer text-muted">
+      <a href="#" class="btn btn-outline-primary">Open Task</a>
+    </div>
+  </div>
+</div>`;
+
+const insertToDOM = (content) => {
+  taskContainer.insertAdjacentHTML("beforeend", content);
 };
